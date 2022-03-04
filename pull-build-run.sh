efeea0f979cdc246a613250c56dbb5edd936fe11
Geoyeob Kim <gasbank@gmail.com>
2022-03-04 16:02:32 +0000

    build script modified.

--- pull-build-run.sh
+++ pull-build-run.sh
@@ -2,5 +2,6 @@
 
 while :
 do
-	git pull && go build && sudo ./cheap-altari-bot /etc/letsencrypt/live/plusalpha.top/fullchain.pem /etc/letsencrypt/live/plusalpha.top/privkey.pem
+	git pull && go build && sudo -E bash -c './cheap-altari-bot /etc/letsencrypt/live/plusalpha.top/fullchain.pem /etc/letsencrypt/live/plusalpha.top/privkey.pem'
+
 done
